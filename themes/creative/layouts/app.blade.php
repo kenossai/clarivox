be0709c1d536f261fead5d4493a2fdd5c5474784
Kenneth Ossai <kenossai1@gmail.com>
2026-06-18 19:50:50 +0100

    Update app.blade.php

--- themes/creative/layouts/app.blade.php
+++ themes/creative/layouts/app.blade.php
@@ -8,33 +8,73 @@
 
     {!! app(\App\Services\SeoService::class)->render() !!}
 
-    {{-- Fonts --}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
-        rel="stylesheet">
+    {{-- Styles --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/swiper-bundle.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/scrollcue.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/remixicon.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/header.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/footer.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/dark-theme.css') }}">
 
-    @vite(['resources/css/app.css'])
-
-    @stack('styles')
 </head>
 
-<body class="antialiased font-sans">
+<body>
 
-    {{-- Navigation --}}
-    @include('creative::partials.nav')
+    <!--  Preloader Start -->
+    {{-- <div class="preloader-area" id="preloader">
+        <div class="spinner">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div> --}}
+    <!--  Preloader End -->
 
-    {{-- Page Content --}}
-    <main>
-        @yield('content')
-    </main>
+    <!-- Theme Switcher Start -->
+    <div class="switch-theme-mode">
+        <label id="switch" class="switch">
+            <input type="checkbox" onchange="toggleTheme()" id="slider">
+            <span class="slider round"></span>
+        </label>
+    </div>
+    <!-- Theme Switcher End -->
 
-    {{-- Footer --}}
-    @include('creative::partials.footer')
+    <!-- Custom Cursor -->
+    <div class="cursor"><span class="cursor-text"></span></div>
+    <div class="cursor-inner"></div>
+    <div id="smooth-wrapper">
+        <div id="smooth-content">
+            @include('creative::partials.nav')
 
-    @vite(['resources/js/app.js'])
+            {{-- Page Content --}}
+            <main>
+                @yield('content')
+            </main>
 
-    @stack('scripts')
+            {{-- Footer --}}
+            @include('creative::partials.footer')
+        </div>
+    </div>
+    {{-- Scripts --}}
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/megamenu.js') }}"></script>
+    <script src="{{ asset('assets/js/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/fslightbox.js') }}"></script>
+    <script src="{{ asset('assets/js/gsap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/scrollTrigger.min.js') }}"></script>
+    <script src="{{ asset('assets/js/lenis.min.js') }}"></script>
+    <script src="{{ asset('assets/js/scrollToPlugin.js') }}"></script>
+    <script src="{{ asset('assets/js/SplitText.min.js') }}"></script>
+    <script src="{{ asset('assets/js/customEase.js') }}"></script>
+    <script src="{{ asset('assets/js/scrollcue.min.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+
+
 </body>
 
 </html>
